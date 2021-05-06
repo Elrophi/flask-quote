7a74d6b06beee76e74757bb91a37e5aced80443f
el <elrophilskwaila@gmail.com>
2021-05-06 19:20:30 +0300

    testing

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 from app import app
 if __name__ == '__main__':
